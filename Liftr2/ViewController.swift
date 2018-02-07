136304fac3610283ddb161e1dc700d87704d5dbe
Connor Berry <connorberry9@icloud.com>
2018-02-07 08:31:09 +0000

    Initial Commit

---
+++ Liftr2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liftr2
+//
+//  Created by Connor Berry on 07/02/2018.
+//  Copyright © 2018 Connor Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
