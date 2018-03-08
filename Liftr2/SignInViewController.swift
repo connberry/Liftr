466c3f901538f4037a0006ddfbd5d90408a40d16
Connor Berry <connorberry9@icloud.com>
2018-03-08 20:15:36 +0000

    Sign up and log in complete

--- Liftr2/SignInViewController.swift
+++ Liftr2/SignInViewController.swift
@@ -8,7 +8,6 @@ import UIKit
 import Firebase
 
 class SignInViewController: UIViewController, UITextFieldDelegate {
-
     
     @IBOutlet weak var EmailAddressTextField: UITextField!
     @IBOutlet weak var PasswordTextField: UITextField!
@@ -17,51 +16,35 @@ class SignInViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    // Background gradient colour, direction and frame
-    let gradientLayer = CAGradientLayer()
-    gradientLayer.frame = self.view.frame
-    gradientLayer.colors = [UIColor(red: 156/255.5, green: 102/255.5, blue: 211/255.5, alpha: 1.0).cgColor, UIColor(red: 249/255.5, green: 122/255.5, blue: 225/255.5, alpha: 1.0).cgColor]
-    gradientLayer.locations = [0.0, 1.0]
-    gradientLayer.startPoint = CGPoint (x: 0.5, y: 1.0)
-    gradientLayer.endPoint = CGPoint (x: 1.0, y: 0.5)
-    self.view.layer.insertSublayer(gradientLayer, at: 0)
+        // Background gradient colour, direction and frame
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.frame
+        gradientLayer.colors = [UIColor(red: 156/255.5, green: 102/255.5, blue: 211/255.5, alpha: 1.0).cgColor, UIColor(red: 249/255.5, green: 122/255.5, blue: 225/255.5, alpha: 1.0).cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint (x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint (x: 1.0, y: 0.5)
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
         
-    EmailAddressTextField.delegate = self
-    PasswordTextField.delegate = self
-        
-    // Start of hide keyboard
-    self.EmailAddressTextField.delegate = self
+        EmailAddressTextField.delegate = self
+        PasswordTextField.delegate = self
     }
     
-    // Do additional tasks associated with presenting the view
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
-    //Log in once details entered
+    
     @IBAction func LogInButton(_ sender: Any) {
-    if (EmailAddressTextField.text != "" && PasswordTextField.text != ""){
-    Auth.auth().signIn(withEmail: EmailAddressTextField.text!, password: PasswordTextField.text!) { user, error in
-    if error == nil {
-    // Signed in successfully
-    let alert = UIAlertController(title: "YAY!", message: "You've signed in! 👏🏻 Press continue...", preferredStyle: .alert)
-    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-    alert.addAction(action)
-    self.present(alert, animated: true, completion: nil) }
-    // Sign in Error
-    DispatchQueue.main.async{
-    let alert = UIAlertController(title: "Oh dear...", message: "You've not got your details right 🤦‍♀️", preferredStyle: .alert)
-    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alert.addAction(action)
-    self.present(alert, animated: true, completion: nil) }
-}
-}
-}
-    // End of hide keyboard when user touches outside keyboard
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    //Presses return key
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool { EmailAddressTextField.resignFirstResponder()
-    return true
+        if (EmailAddressTextField.text != "" && PasswordTextField.text != ""){
+            Auth.auth().signIn(withEmail: EmailAddressTextField.text!, password: PasswordTextField.text!) { user, error in
+                if error == nil {
+                    self.performSegue(withIdentifier: "LogInSegue", sender: nil)
+                } else {
+                    let alert = UIAlertController(title: "Oh dear...", message: "You've gone and entered incorrect details, try harder next time! 🤦‍♀️", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(action)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 }
--- Liftr2/SignUpViewController.swift
+++ Liftr2/SignUpViewController.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import Firebase
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
